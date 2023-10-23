910ba336eb5e50e2f8465bf8685b54ad8d23cbb4
SaaraL <saara@leppihalme.fi>
2023-10-23 13:21:44 +0300

    Add project files.

---
+++ LinqCheatSheet/Lawyer.cs
@@ -0,0 +1,8 @@
+﻿namespace LinqCheatSheet;
+
+public class Lawyer
+{
+    public List<Case> Cases { get; set; } = default;
+    public string FirstName { get; set; } = default;
+    public int MyProperty { get; set; } = default;
+}
